6e49a388538cb0d38840310411a3f7707661df5f
r1phord <vlad.gorenkov.1996@mail.ru>
2020-06-27 02:09:48 +0200

    create views

--- vacancies/views.py
+++ vacancies/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class CompanyView(View):
+    def get(self, request, company_id):
+        return render(request, 'company.html')
+
+
+class AllVacanciesView(View):
+    def get(self, request):
+        return render(request, 'vacancies.html')
+
+
+class CategoryVacanciesView(View):
+    def get(self, request, category):
+        return render(request, 'vacancies.html')
+
+
+class VacancyView(View):
+    def get(self, request, vacancy_id):
+        return render(request, 'vacancy.html')
