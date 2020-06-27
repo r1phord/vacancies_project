4a322fd1b710f2fa5f4e5068b5b381835989571d
r1phord <vlad.gorenkov.1996@mail.ru>
2020-06-28 00:28:39 +0200

    named urls and add links to header

--- st_vacancies/urls.py
+++ st_vacancies/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from vacancies.views import MainView, CompanyView, AllVacanciesView, VacancyView, CategoryVacanciesView
 
 urlpatterns = [
-    path('', MainView.as_view()),
-    path('vacancies/', AllVacanciesView.as_view()),
-    path('vacancies/<int:vacancy_id>/', VacancyView.as_view()),
-    path('vacancies/cat/<str:category>/', CategoryVacanciesView.as_view()),
-    path('companies/<int:company_id>/', CompanyView.as_view())
+    path('', MainView.as_view(), name='main'),
+    path('vacancies/', AllVacanciesView.as_view(), name='vacancies'),
+    path('vacancies/<int:vacancy_id>/', VacancyView.as_view(), name='vacancy'),
+    path('vacancies/cat/<str:category>/', CategoryVacanciesView.as_view(), name='cat_vacancy'),
+    path('companies/<int:company_id>/', CompanyView.as_view(), name='company')
 ]
