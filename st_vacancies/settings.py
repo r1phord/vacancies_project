e1cd7d93ed7f2dbe67c1c96a6638d4dbb08b62f8
r1phord <vlad.gorenkov.1996@mail.ru>
2020-06-28 14:03:43 +0200

    add missing links, add back link, set dubug to false

--- st_vacancies/settings.py
+++ st_vacancies/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@)9_d#ky&_@qoerwjepjkl61o)iywx30pu2t8+@-!*0+^u@n(&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -53,8 +53,7 @@ ROOT_URLCONF = 'st_vacancies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
