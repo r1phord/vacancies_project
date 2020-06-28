bc3729cfb013b6ee4114faffec794090022185c1
r1phord <vlad.gorenkov.1996@mail.ru>
2020-06-28 02:49:35 +0200

    make dinamik index page, vacancies page, add information to views modified company view and template

--- st_vacancies/urls.py
+++ st_vacancies/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from vacancies.views import MainView, CompanyView, AllVacanciesView, VacancyView, CategoryVacanciesView
+from vacancies.views import MainView, CompanyView, AllVacanciesView, VacancyView, SpecialtyVacanciesView
 
 urlpatterns = [
     path('', MainView.as_view(), name='main'),
     path('vacancies/', AllVacanciesView.as_view(), name='vacancies'),
     path('vacancies/<int:vacancy_id>/', VacancyView.as_view(), name='vacancy'),
-    path('vacancies/cat/<str:category>/', CategoryVacanciesView.as_view(), name='cat_vacancy'),
+    path('vacancies/cat/<str:specialty_code>/', SpecialtyVacanciesView.as_view(), name='cat_vacancy'),
     path('companies/<int:company_id>/', CompanyView.as_view(), name='company')
 ]
--- vacancies/views.py
+++ vacancies/views.py
@@ -2,43 +2,61 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.views import View
 
-from vacancies.models import Company, Vacancy
+from vacancies.models import Company, Vacancy, Specialty
 
 
 class MainView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        specialties = Specialty.objects.all()
+        companies = Company.objects.all()
+        return render(request, 'index.html', context={
+            'specialties': specialties,
+            'companies': companies
+        })
 
 
 class CompanyView(View):
     def get(self, request, company_id):
-        company = Company.objects.filter(id=company_id)
-        if len(company) == 0:
+        if not Company.objects.filter(id=company_id).exists():
             return HttpResponseNotFound('company not found')
-        company = company[0]
-        company_vacancies = Vacancy.objects.filter(company=company)
+
+        company = Company.objects.filter(id=company_id)[0]
+
         return render(request, 'company.html', context={
-            'company': company,
-            'company_vacancies': company_vacancies
+            'company': company
         })
 
 
 class AllVacanciesView(View):
     def get(self, request):
-        return render(request, 'vacancies.html')
+        vacancies = Vacancy.objects.all()
+        return render(request, 'vacancies.html', context={
+            'title': 'Все вакансии',
+            'vacancies': vacancies
+        })
 
 
-class CategoryVacanciesView(View):
-    def get(self, request, category):
-        return render(request, 'vacancies.html')
+class SpecialtyVacanciesView(View):
+    def get(self, request, specialty_code):
+        if not Specialty.objects.filter(code=specialty_code).exists():
+            return HttpResponseNotFound('no such specialty')
+
+        spec = Specialty.objects.filter(code=specialty_code)[0]
+        vacancies = Vacancy.objects.filter(specialty=spec)
+
+        return render(request, 'vacancies.html', context={
+            'title': spec.title,
+            'vacancies': vacancies
+        })
 
 
 class VacancyView(View):
     def get(self, request, vacancy_id):
-        vacancy = Vacancy.objects.filter(id=vacancy_id)
-        if len(vacancy) == 0:
+        if not Vacancy.objects.filter(id=vacancy_id).exists():
             return HttpResponseNotFound('vacancy not found')
-        vacancy = vacancy[0]
+
+        vacancy = Vacancy.objects.filter(id=vacancy_id)[0]
+
         return render(request, 'vacancy.html', context={
             'vacancy': vacancy
         })
